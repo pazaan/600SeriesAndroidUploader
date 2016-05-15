b0cad169b2cb77c8d05d5cb7cbab2f4711260c7d
Lennart Goedhart <lennart@omnibase.com.au>
2016-05-15 16:07:52 +1000

    Though I couldn't repro #2, seeing if this helps @wright-david.

--- app/src/main/java/info/nightscout/android/medtronic/GetHmacAndKeyActivity.java
+++ app/src/main/java/info/nightscout/android/medtronic/GetHmacAndKeyActivity.java
@@ -112,7 +112,7 @@ public class GetHmacAndKeyActivity extends Activity implements LoaderCallbacks<C
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+        if (TextUtils.isEmpty(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
