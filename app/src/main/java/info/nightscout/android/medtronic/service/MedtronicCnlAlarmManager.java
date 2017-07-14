fa0cd193b783842b740d7a94305ea714ffac44b3
Pogman <subaziro@gmail.com>
2017-07-14 01:00:24 +0100

    Due to potential of some versions of android to mangle alarms and clash with polling times the default alarm reset is set to POLL_PERIOD_MS + 60 seconds
      It's expected to trigger between polls if alarm has not been honored with a safe margin greater then the around 10 minutes that some OS versions force during sleep

--- app/src/main/java/info/nightscout/android/medtronic/service/MedtronicCnlAlarmManager.java
+++ app/src/main/java/info/nightscout/android/medtronic/service/MedtronicCnlAlarmManager.java
@@ -66,8 +66,13 @@ public class MedtronicCnlAlarmManager {
 
     // restarting the alarm after MedtronicCnlIntentService.POLL_PERIOD_MS from now
     public static void restartAlarm() {
-        //setAlarmAfterMillis(MainActivity.pollInterval + MedtronicCnlIntentService.POLL_GRACE_PERIOD_MS);
-        setAlarmAfterMillis(ConfigurationStore.getInstance().getPollInterval()); // grace already accounted for when using current intent time to set default restart
+//        setAlarmAfterMillis(ConfigurationStore.getInstance().getPollInterval()); // grace already accounted for when using current intent time to set default restart
+
+        // Due to potential of some versions of android to mangle alarms and clash with polling times
+        // the default alarm reset is set to POLL_PERIOD_MS + 60 seconds
+        // It's expected to trigger between polls if alarm has not been honored with a safe margin greater then
+        // the around 10 minutes that some OS versions force during sleep
+        setAlarmAfterMillis(MedtronicCnlIntentService.POLL_PERIOD_MS + 60000L); // grace already accounted for when using current intent time to set default restart
     }
 
     // Cancel the alarm.
