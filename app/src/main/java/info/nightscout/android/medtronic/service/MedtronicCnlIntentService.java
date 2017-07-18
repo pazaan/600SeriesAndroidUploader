3f67a00e19f5e05c35dc166808c21d4363692cf6
Pogman <subaziro@gmail.com>
2017-07-18 18:45:32 +0100

    Fix Polling issues

--- app/src/main/java/info/nightscout/android/medtronic/service/MedtronicCnlIntentService.java
+++ app/src/main/java/info/nightscout/android/medtronic/service/MedtronicCnlIntentService.java
@@ -141,9 +141,10 @@ CNL: unpaired PUMP: unpaired UPLOADER: unregistered = "Invalid message received 
 
             final long timePollStarted = System.currentTimeMillis();
 
-            long timeLastGoodSGV = 0;
-            if (dataStore.getLastPumpStatus().getSgv() > 0) {
-                timeLastGoodSGV = dataStore.getLastPumpStatus().getSgvDate().getTime();
+            long timeLastGoodSGV = dataStore.getLastPumpStatus().getSgvDate().getTime();
+            if (dataStore.getLastPumpStatus().getSgv() == 0
+                || timePollStarted - timeLastGoodSGV > 24 * 60 * 60 * 1000) {
+                timeLastGoodSGV = 0;
             }
 
             final long timePollExpected;
@@ -381,6 +382,7 @@ CNL: unpaired PUMP: unpaired UPLOADER: unregistered = "Invalid message received 
         }
     }
 
+    // TODO - Refactor polling system and make super clear how polling is calculated and why certain precautions are needed
     private void scheduleNextPoll(long timePollStarted, long timeLastGoodSGV, long pollInterval) {
         // smart polling and pump-sensor poll clash detection
         long now = System.currentTimeMillis();
@@ -390,6 +392,10 @@ CNL: unpaired PUMP: unpaired UPLOADER: unregistered = "Invalid message received 
         }
         long nextActualPollTime = lastActualPollTime + POLL_PERIOD_MS;
         long nextRequestedPollTime = lastActualPollTime + pollInterval;
+        // check if request is really needed
+        if (nextRequestedPollTime - now < 10000L) {
+            nextRequestedPollTime = nextActualPollTime;
+        }
         // extended unavailable SGV may be due to clash with the current polling time
         // while we wait for a good SGV event, polling is auto adjusted by offsetting the next poll based on miss count
         if (dataStore.getUnavailableSGVCount() > 0) {
@@ -403,8 +409,8 @@ CNL: unpaired PUMP: unpaired UPLOADER: unregistered = "Invalid message received 
             }
         }
         // check if requested poll time is too close to next actual poll time
-        if (nextRequestedPollTime - now > nextActualPollTime - now - POLL_PRE_GRACE_PERIOD_MS
-                && nextRequestedPollTime - now < nextActualPollTime - now + POLL_GRACE_PERIOD_MS) {
+        if (nextRequestedPollTime > nextActualPollTime - POLL_GRACE_PERIOD_MS - POLL_PRE_GRACE_PERIOD_MS
+                && nextRequestedPollTime < nextActualPollTime) {
             nextRequestedPollTime = nextActualPollTime;
         }
         MedtronicCnlAlarmManager.setAlarm(nextRequestedPollTime);
